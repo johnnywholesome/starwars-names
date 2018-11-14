ff52fc1e394f1f138519bfd72d73b3ba7d1dd548
Jon Schmedt <jon.schmedt@usfoods.com>
2018-11-14 16:28:55 -0600

    added library

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+var uniqueRandomArray = require('unique-random-array');
+var starWarsNames = require('./star-wars-names.json');
+
+const getRandomItem = uniqueRandomArray(starWarsNames);
+
+module.exports = {
+  all: starWarsNames,
+  random: random
+};
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  } else {
+    const randomItems = [];
+    for (let i = 0; i < number; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
